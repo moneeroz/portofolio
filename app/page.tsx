9685c9ef0f8a9ec3e8bd3affa774c30ab7a06950
Moneer <moneer.alromimah@gmail.com>
2023-08-17 22:44:34 -0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,17 @@
-import { AiOutlineMail } from "react-icons/ai";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p2 flex justify-center items-center">
-        <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
-            let&apos;s build something together
-          </p>
-          <h1 className="py-4 text-gray-700">
-            Hi, I&apos;m <span className="text-[#5651E5]">Moneer</span>
-          </h1>
-          <h1 className="py-2 text-gray-700">A Full-Stack Web Developer</h1>
-          <p className="py-4 m-auto max-w-[70%] text-gray-600">
-            I've shifted from Chemical Engineering to become a full stack web
-            developer, drawn by Software Development's evolving nature, complex
-            challenges, and problem-solving opportunities. My commitment to
-            continuous self-improvement is evident in my exploration of diverse
-            subjects during leisure moments. This drive for growth is mirrored
-            in both my expanding knowledge and my consistent progress in
-            delivering higher quality work.
-          </p>
-
-          <div className="flex justify-between items-center max-w-[330px] m-auto py-4">
-            <div className="p-5 rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-125 ease-in duration-300">
-              <FaLinkedinIn />
-            </div>
-            <div className="p-5 rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-125 ease-in duration-300">
-              <FaGithub />
-            </div>
-            <div className="p-5 rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-125 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-            <div className="p-5 rounded-full shadow-lg shadow-gray-400 cursor-pointer hover:scale-125 ease-in duration-300">
-              <BsFillPersonLinesFill />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className="w-full h-screen text-center">
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
   );
 }
